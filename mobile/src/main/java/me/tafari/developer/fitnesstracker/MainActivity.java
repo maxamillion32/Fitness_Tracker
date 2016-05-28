6e529722ec1e0aee1d6a312a107e90ceea206dc4
takodatafari <takodatafari@gmail.com>
2016-05-28 14:59:20 -0700

    Added display activity

--- mobile/src/main/java/me/tafari/developer/fitnesstracker/MainActivity.java
+++ mobile/src/main/java/me/tafari/developer/fitnesstracker/MainActivity.java
@@ -1,10 +1,12 @@
 package me.tafari.developer.fitnesstracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+
+    public void display(View view) {
+        Log.d("Debug Tag: ", "DisplayActivity Intent has been launched");
+        Intent intent = new Intent(this, DisplayActivity.class);
+        startActivity(intent);
     }
 
     @Override
